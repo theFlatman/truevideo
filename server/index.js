05c01ef536005d3b684babbdf0671b987df14705
Patrick Flattich <patrickflattich@pop-os.localdomain>
2020-03-17 16:32:17 +0100

    restructure

--- server/index.js
+++ server/index.js
@@ -1,16 +1,18 @@
-const config = require('./config');
-const express = require('express');
-const bodyParser = require('body-parser');
-const pino = require('express-pino-logger')();
-const { videoToken } = require('./tokens');
+const config = require("./config");
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const pino = require("express-pino-logger")();
+const { videoToken } = require("./tokens");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(pino);
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const sendTokenResponse = (token, res) => {
-  res.set('Content-Type', 'application/json');
+  res.set("Content-Type", "application/json");
   res.send(
     JSON.stringify({
       token: token.toJwt()
@@ -18,26 +20,24 @@ const sendTokenResponse = (token, res) => {
   );
 };
 
-app.get('/api/greeting', (req, res) => {
-  const name = req.query.name || 'World';
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
-});
-
-app.get('/video/token', (req, res) => {
+app.get("/video/token", (req, res) => {
   const identity = req.query.identity;
   const room = req.query.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
-
 });
-app.post('/video/token', (req, res) => {
+app.post("/video/token", (req, res) => {
   const identity = req.body.identity;
   const room = req.body.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
-);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`TrueVideo running on ${port}`);
